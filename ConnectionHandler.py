28d4709c1b42d3b006107112ca46163f1adf0dd0
Or Shafran <orshafran@gmail.com>
2023-12-04 00:15:00 +0200

    added verbosity

--- ConnectionHandler.py
+++ ConnectionHandler.py
@@ -5,7 +5,6 @@ class ConnectionHandler:
         pass
 
     def test_connection(self, interface: str, client: GtaClient, route: GtaClient)->bool:
-        print(f"Tested connection for client: {client.identifier} route: {route.ip}")
         return False
 
     def connect(self, interface: str, client: GtaClient, route: GtaClient)->bool:
